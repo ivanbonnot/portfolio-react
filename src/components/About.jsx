d082104334f67f23fe9adec6eeace09515bb18b3
ivanbonnot <ivanbonnot@gmail.com>
2023-07-04 20:59:43 -0300

    listo about

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+                </div>
+                <p className='text-xl mt-20'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quae, accusamus saepe voluptatum delectus adipisci dignissimos rem deserunt, reprehenderit facere porro similique nobis mollitia inventore ratione, cumque dolor non sequi impedit! Cupiditate, tenetur necessitatibus. Doloremque, iure laboriosam consequatur maxime est aliquid reiciendis quae ad? Vitae doloribus incidunt optio nisi non.
+                </p>
+
+                <br />
+
+                <p className='text-xl'>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo labore incidunt ad hic molestiae, totam sapiente consequatur soluta mollitia, dolore culpa pariatur atque impedit, porro rerum ab accusamus doloribus perferendis vero cupiditate! Fugiat necessitatibus voluptate sunt quo, voluptatibus tenetur soluta aliquid explicabo blanditiis error mollitia eligendi recusandae corrupti porro? Nulla.
+                </p>
+
+            </div>
+        </div>
+    )
+}
+
+export default About
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -19,42 +19,39 @@ const SocialLinks = () => {
       child: (
         <>  Github < FaGithub size={30} /> </>
       ),
-      href: 'https://github.com/ivanbonnot?tab=repositories',
-      style: 'rounded-tr-md'
+      href: 'https://github.com/ivanbonnot?tab=repositories'
     },
     {
       id: 3,
       child: (
         <>  Mail < HiOutlineMail size={30} /> </>
       ),
-      href: 'https://www.linkedin.com/in/ivanbonnot/',
-      style: 'rounded-tr-md'
+      href: 'mailto: ivanbonnot@gmail.com',
     },
     {
       id: 4,
       child: (
-        <>  CV < BsFillPersonLinesFill size={30} /> </>
+        <>  Resume < BsFillPersonLinesFill size={30} /> </>
       ),
       href: '/resume.pdf',
-      style: 'rounded-tr-md',
+      style: 'rounded-br-md',
       download: true,
     }
   ]
 
   return (
-    <div className='flex flex-col top-[35%] left-0 fixed'>
+    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
       <ul>
         {links.map(({ id, child, href, style, download }) =>
-          <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + "" + style }>
+          <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + style}>
             <a href={href} className='flex justify-between items-center w-full text-white'
-            download={{download}}
-            target='_blank'
-            rel='noreferrer'>
+              download={{ download }}
+              target='_blank'
+              rel='noreferrer'>
               {child}
             </a>
           </li>
         )}
-
       </ul>
     </div>
   )
