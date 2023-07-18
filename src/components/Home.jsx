fe54fe999ba2655bf5a17d35320064a50041cbf5
ivanbonnot <ivanbonnot@gmail.com>
2023-07-18 19:30:27 -0300

    listo responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,8 @@ import {Link} from 'react-scroll'
 
 const Home = () => {
     return (
-        <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 pt-14'>
-            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+        <div name="home" className='h-full w-full bg-gradient-to-b from-black via-black to-gray-800 pt-14 '>
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full pt-10 px-4 md:flex-row'>
                 <div className='flex flex-col justify-center h-full'>
                     <h2 className='text-4xl sm:text-6xl font-bold text-white'>
                         Hola, soy un desarrollador Full Stack
@@ -17,7 +17,7 @@ const Home = () => {
                     <div>
                         <Link to="portfolio" smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                             Portfolio
-                            <span className='group-hover:rotate-90 duration-300'>
+                            <span className='group-hover:rotate-90 group-target:rotate-90 duration-300'>
                                 < MdOutlineKeyboardArrowRight size={25} className='ml-1' />
                             </span>
                         </Link>
@@ -25,7 +25,7 @@ const Home = () => {
 
                 </div>
                 <div>
-                    <img src={HeroImage} alt="my profile" className='rounded-2xl mx-auto w-2/3 md:w-full border-2 border-gray-100' />
+                    <img src={HeroImage} alt="my profile" className='mt-8 rounded-2xl mx-auto w-2/3 md:w-full border-2 border-gray-100' />
                 </div>
             </div>
         </div>
