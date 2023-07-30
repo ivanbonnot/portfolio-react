eda506b77f68f35b6ffe24fffb1583b85a1b3205
ivanbonnot <ivanbonnot@gmail.com>
2023-07-30 14:40:18 -0300

    lista traducciones, falta cambiar foto y armar texto home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import { FormattedMessage } from 'react-intl'
 
 const Home = () => {
     return (
-        <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 pt-14 '>
+        <div name="home" className='lg:h-screen h-full w-full bg-gradient-to-b from-black via-black to-gray-800 pt-14 '>
             <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center pt-10 px-4 md:flex-row'>
                 <div className='flex flex-col justify-center h-full'>
                     <h2 className='text-4xl mr-3 sm:text-6xl font-bold text-white'>
